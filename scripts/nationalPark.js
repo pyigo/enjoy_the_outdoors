600452328c6f5a91dcbc0da6275b276baa343994
paulette <paulette.yigo@gmail.com>
2023-05-24 15:15:41 -0400

    created element and loop to get parks by type

--- scripts/nationalPark.js
+++ scripts/nationalPark.js
@@ -1,11 +1,22 @@
-// filter
-const parkByLocation = document.getElementById("parkByLocation");
-// iterate through location array from data file
-for (i=0; i<locationsArray.length; i++){
+// filter by location
+const parkByLocation = document.getElementById("parksByLocation");
+const parkByType = document.getElementById("parksByType")
+ 
+// iterate through location array from locationData.js file
+for (let i=0; i<locationsArray.length; i++){
     let parkLocation = locationsArray[i];
     const option = document.createElement("option")
     option.innerHTML=parkLocation;
     option.value=parkLocation;
     parkByLocation.appendChild(option);
 }
+
+// iterate through type array from parkTypeData.js
+ for (let i =0; i<parkTypesArray.length; i++){
+    let parkType = parkTypesArray[i];
+    const option = document.createElement("option");
+    option.innerHTML=parkType;
+    option.value=parkType;
+    parkByType.appendChild(option);
+ }
 
