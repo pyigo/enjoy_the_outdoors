097ef98ed4041ee4b754941bcc2b8b3099249438
paulette <paulette.yigo@gmail.com>
2023-05-25 12:39:30 -0400

    displaying mountains list from dropdown

--- scripts/mountainsInfo.js
+++ scripts/mountainsInfo.js
@@ -3,5 +3,9 @@ const mountains = document.getElementById("mountainsList")
 // iterate over mountains data file
 for (let i=0; i<mountainsArray.length; i++){
     let mountain = mountainsArray[i];
-    console.log(i)
+    const option = document.createElement("option");
+    option.innerHTML = mountain.name;
+    option.value = mountain.name;
+    mountains.appendChild(option);
+
 }
